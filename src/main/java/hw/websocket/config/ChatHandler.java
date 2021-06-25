1d3166c946a7921f41e3cee30c0ab00fd1199f30
Hyeongwon Lee <leehwdatabase@gmail.com>
2021-06-25 15:33:18 +0900

    only websocket simple example

--- src/main/java/hw/websocket/config/ChatHandler.java
+++ src/main/java/hw/websocket/config/ChatHandler.java
@@ -1,7 +1,12 @@
 package hw.websocket.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hw.websocket.ChatMessage;
+import hw.websocket.ChatRoom;
+import hw.websocket.ChatService;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
@@ -9,38 +14,20 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-@Component
 @Slf4j
-public class ChatHandler extends TextWebSocketHandler {
-
-  private static List<WebSocketSession> webSocketSessionList = new ArrayList<>();
+@RequiredArgsConstructor
+@Component
+class ChatHandler extends TextWebSocketHandler {
+  private final ObjectMapper objectMapper;
+  private final ChatService chatService;
 
   @Override
-  protected void handleTextMessage(WebSocketSession webSocketSession, TextMessage message) throws Exception {
+  protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
     String payload = message.getPayload();
-    log.info("payload: {}", payload);
+    log.info("payload {}", payload);
 
-    for (WebSocketSession session : webSocketSessionList) {
-      session.sendMessage(message);
-    }
+    ChatMessage chatMessage = objectMapper.readValue(payload, ChatMessage.class);
+    ChatRoom room = chatService.findRoomById(chatMessage.getRoomId());
+    room.handleActions(session, chatMessage, chatService);
   }
-
-
-  // client 가 접속 시 호출되는 메서드
-  @Override
-  public void afterConnectionEstablished(WebSocketSession webSocketSession) throws Exception {
-
-    webSocketSessionList.add(webSocketSession);
-    log.info(webSocketSession + "클라이언트 접속");
-  }
-
-  // client 가 접속 해제시 호출되는 메서드
-
-  @Override
-  public void afterConnectionClosed(WebSocketSession webSocketSession, CloseStatus closeStatus) throws Exception {
-    log.info(webSocketSession + "클라이언트 접속 해제");
-    webSocketSessionList.remove(webSocketSessionList);
-
-  }
-
 }
